dc6c71a6f32de07e878e8709ec3670be41c3b347
Rishabh Mishra <rishabhmishra3321@gmail.com>
2021-05-29 23:02:56 +0530

    Mobile fix @long-press touch events

--- src/components/Keys/Multiple/index.jsx
+++ src/components/Keys/Multiple/index.jsx
@@ -9,13 +9,24 @@ function MultipleKey({
   return (
     <div className="p-2 select-none">
       <div
-        onClick={() => onClickHandler(data.id)}
+        onClick={() => {
+          console.log("Clicking");
+          onClickHandler(data.id);
+        }}
         onMouseDown={(e) => {
           e.preventDefault();
           onMouseDownHandler(data.id);
         }}
         onMouseUp={(e) => {
           e.preventDefault();
+          onMouseUpHandler(data.id);
+        }}
+        onTouchStart={(e) => {
+          console.log("Start");
+          onMouseDownHandler(data.id);
+        }}
+        onTouchEnd={() => {
+          console.log("END");
           onMouseUpHandler(data.id);
         }}
         className="active:bg-orange-400 active:text-orange-100 w-full h-20 border-2 font-mono font-bold text-lg text-orange-800 border-orange-900 rounded-3xl flex justify-center items-center cursor-pointer flex-col leading-none"
